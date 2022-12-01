ef3051de8fd536f8b8ddb44358f5f4e69f25296a
Tuur Bressinck <Tuur.BRESSINCK@sfpd.fgov.be>
2022-12-01 09:11:54 +0100

    A bit simpler

--- day01/tuur/DayOne.java
+++ day01/tuur/DayOne.java
@@ -4,41 +4,36 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class DayOne {
 	public static void main(String[] args) throws IOException, URISyntaxException {
 		List<String> lines = Files.readAllLines(Paths.get(DayOne.class.getResource("input-part1.txt").toURI()));
 		
-		int current=0, max=0;
+		int current=0, max1=0, max2=0, max3=0;
 		for(String line : lines) {
 			if (line.trim().isEmpty()) {
-				if(max < current) {
-					max = current;
+
+				if(max3 < current) {
+					max3 = current;
+					if(max2 < max3) {
+						int tmp = max2;
+						max2 = max3;
+						max3 = tmp;
+						if(max1 < max2) {
+							tmp = max1;
+							max1 = max2;
+							max2 = tmp;
+						}
+					}
 				}
+				
 				current = 0;
 			} else {
 				current += Integer.parseInt(line);
 			}
 		}
-		System.out.println("Part 1: Calories = " + max);
-		
-		List<Integer> counts = new ArrayList<>();
-		current=0;
-		for(String line : lines) {
-			if (line.trim().isEmpty()) {
-				counts.add(current);
-				current = 0;
-			} else {
-				current += Integer.parseInt(line);
-			}
-		}
-		
-		Collections.sort(counts);
-		int total = counts.get(counts.size()-1) + counts.get(counts.size()-2) + counts.get(counts.size()-3);
-		System.out.println("Part 2: Calories = " + total);
-		
+		System.out.println("Part 1: Calories = " + max1);
+		System.out.println("Part 2: Calories = " + (max1 + max2 + max3));		
 	}
 }
