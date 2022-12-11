487199eedbd8eb8cfaa6629e0f9361a21671863f
Nabochi <koen.dhaen@gmail.com>
2022-12-11 20:26:37 +0100

    bazinga

--- day11/koen/src/main.rs
+++ day11/koen/src/main.rs
@@ -21,8 +21,8 @@ lazy_static! {
 }
 
 fn main() {
-    // let path = String::from("data/input.txt");
-    let path = String::from("data/input_test.txt");
+    let path = String::from("data/input.txt");
+    // let path = String::from("data/input_test.txt");
     let mut monkeys = parse_input(&path);
     // let monkey_business = do_rounds(&mut monkeys, 20, 3);
     let monkey_business = do_rounds(&mut monkeys, 10000, 1);
@@ -82,7 +82,12 @@ fn parse_monkey(monkey_string: &String) ->  Monkey {
 }
 
 fn do_rounds(monkeys: &mut HashMap<u128, Monkey>, n: u128, correction: u128) -> u128 {
-    // let mut monkeys = monkeys.clone();
+    let mut params: Vec<u128> = monkeys.values().map(|x| x.param).filter(|x| x > &0).collect();
+    let mut divisors: Vec<u128> = monkeys.values().map(|x| x.divisor).filter(|x| x > &0).collect();
+    params.append(&mut divisors);
+    params.sort();
+    params.dedup();
+    let modulo: u128 = params.iter().product();
     let m = monkeys.len() as u128;
     // println!("Initial state");
     // for (key, val) in monkeys.iter() {
@@ -94,7 +99,7 @@ fn do_rounds(monkeys: &mut HashMap<u128, Monkey>, n: u128, correction: u128) -> 
         }
         for monkey_id in 0 .. m {
             let monkey = monkeys.get_mut(&monkey_id).expect("REASON");
-            let items_to = monkey.inspect_items(&correction);
+            let items_to = monkey.inspect_items(&correction, &modulo);
             for item_to in items_to {
                 monkeys.get_mut(&item_to[1]).expect("REASON").items.push(item_to[0]);
             }
@@ -127,19 +132,20 @@ impl Monkey {
         (self.operation)(*level, a)
     }
 
-    fn inspect_item(&self, level: &u128, correction: &u128) -> [u128; 2] {
+    fn inspect_item(&self, level: &u128, correction: &u128, modulo: &u128) -> [u128; 2] {
         let mut new_level = self.do_operation(&level);
         new_level = new_level / correction;
         let to = match new_level % self.divisor {
             0 => self.to[0],
             _ => self.to[1]
         };
+        new_level = new_level % modulo;
         [new_level, to]
     }
-    fn inspect_items(&mut self, correction: &u128) -> Vec<[u128; 2]> {
+    fn inspect_items(&mut self, correction: &u128, modulo: &u128) -> Vec<[u128; 2]> {
         let mut items_to: Vec<[u128; 2]> = vec![];
         for item in &self.items {
-            items_to.push(self.inspect_item(&item, correction));
+            items_to.push(self.inspect_item(&item, correction, modulo));
             self.inspected += 1;
         }
         self.items.clear();
